6bec25852d8c37a1a344a1b74fc0af91beb281bc
Javlonbek Mingboev <javlonbekmingbayev3@gmail.com>
2026-05-05 18:27:28 +0500

    Implement SMILES input validation in main function
    Added input validation for SMILES format in main function.

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from utils import methyl_swapping, carb_acid_to_tetrazole, halogen_scan
+from rdkit import Chem
 
 def main():
     print("-- Welcome to Bioisostere Swapper App.")
-    molecule = input("Enter a molecule in SMILES format: ")
+    while True:
+        molecule = input("Enter a molecule in SMILES format: ")
+        mol = Chem.MolFromSmiles(molecule)
+        if mol is not None:
+            break
+        else:
+            print("Invalid SMILES format input. Try again.")
     print("Menu:")
     print("1. Methyl Swapping")  
     print("2. Substituting carboxylic acid group with a tetrazole ring.")
